a2954c1e97b0579b16421bb30bb2a9655efcff6e
Cole Miller <m@cole-miller.net>
2025-07-30 12:30:35 -0400

    Remove remnants of webrtc_dir caching (#7)
    Ensure that we download the webrtc release anew each time
    download_webrtc is called (from either the livekit-ffi build script or
    the webrtc-sys build script). This is another stab at fixing the build
    errors we've been seeing in Zed CI, or at least making them easier to
    understand.
    
    Also reverts #6 since it seems the integrity of the downloaded zip was a
    red herring.

--- livekit-ffi/build.rs
+++ livekit-ffi/build.rs
@@ -20,8 +20,10 @@ fn main() {
     }
 
     let webrtc_dir = webrtc_sys_build::webrtc_dir();
+    println!("cargo:warning=livekit-ffi downloading webrtc to {webrtc_dir:?}");
+    webrtc_sys_build::download_webrtc().unwrap();
     if !webrtc_dir.exists() {
-        webrtc_sys_build::download_webrtc().unwrap();
+        panic!("download_webrtc didn't create webrtc_dir")
     }
 
     {
--- webrtc-sys/build.rs
+++ webrtc-sys/build.rs
@@ -85,8 +85,10 @@ fn main() {
     let webrtc_include = webrtc_dir.join("include");
     let webrtc_lib = webrtc_dir.join("lib");
 
+    println!("cargo:warning=webrtc-sys downloading webrtc to {webrtc_dir:?}");
+    webrtc_sys_build::download_webrtc().unwrap();
     if !webrtc_dir.exists() {
-        webrtc_sys_build::download_webrtc().unwrap();
+        panic!("download_webrtc didn't create webrtc_dir")
     }
 
     builder.includes(&[
